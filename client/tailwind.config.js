0d4d4524fcf3e5dc9113ed9a6fa85ddd66d71681
guillenec <guillermoneculqueo@gmail.com>
2023-11-28 20:28:45 -0300

    style: Configurar estilos personalizados con Tailwind CSS y ajustes en fuentes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,59 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;700&family=Poppins:wght@400;500;600;700&display=swap');
+
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+
+:root {
+  font-family: 'Poppins', sans-serif;
+  /* line-height: 1.5; */
+  font-weight: 400;
+
+  color-scheme: light dark;
+  color: #e7a56f;
+  background-color: #4d494a;
+
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  --titulo: 'Nunito', sans-serif;
+  --parrafo: 'Poppins', sans-serif;
+
+  --colorCustom1: #f1b95e;
+  --colorCustom2: #ff6347;
+  --colorCustom3: #b2e43f;
+  --colorCustom4: #333333;
+  --colorCustom5: #f8f9fa;
 }
+
+a {
+  font-weight: 500;
+  text-decoration: inherit;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  display: flex;
+  place-items: center;
+  min-width: 320px;
+  min-height: 100vh;
+}
+
+/* h1 {
+  font-size: 3.2em;
+  line-height: 1.1;
+} */
+
+
+@media (prefers-color-scheme: light) {
+  :root {
+    background-color: #ffffff;
+  }
+}
+

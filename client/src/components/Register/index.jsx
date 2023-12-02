1cf464fbfea68852c4ce0b8a77370e572c4f69c1
Edu568 <eduardo.arevalo072@gmail.com>
2023-12-02 01:12:15 -0300

    Feat: Add  a new register form.

--- client/src/components/Register/index.jsx
+++ client/src/components/Register/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
+  const Navigate = useNavigate()
+
+  const handleRegisterForm = () => {
+    Navigate('/login')
+  }
+
   return (
     <div className='relative bg-colorCustom1 w-[100%] px-6 lg:bg-slate-400 h-screen  p-0 flex flex-col lg:flex-row lg:justify-around gap-8 min-w-[300px]'>
       <div className='lg:flex-col lg:self-center'>
@@ -13,21 +20,17 @@ const Register = () => {
       </div>
       <div className='pt-9 lg:self-center'>
         <form action=''>
-          <div className='py-5'>
-            <input className='rounded lg:w-80 w-full h-[35px] lg:h-[40px]' type='text' name='' id='' placeholder='Nombre' /> <br />
+          <div className='flex flex-col gap-8'>
+            <input className='rounded lg:w-80 w-full h-[35px] lg:h-[40px]' type='text' name='' id='' placeholder='Nombre' />
+            <input className='rounded lg:w-80 w-full h-[35px] lg:h-[40px]' type='text' name='' id='' placeholder='Apelldo' /> 
+            <input className='rounded lg:w-80 w-full h-[35px] lg:h-[40px]' type='email' name='' id='' placeholder='Email' /> 
+            <input className='rounded lg:w-80 w-full h-[35px]' type='password' name='' id='' placeholder='Contraseña' />
+            <input className='rounded lg:w-80 w-full h-[35px]' type='password' name='' id='' placeholder='Confirmar Contraseña' />
+
           </div>
-          <div className='py-5'>
-            <input className='rounded lg:w-80 w-full h-[35px] lg:h-[40px]' type='text' name='' id='' placeholder='Apelldo' /> <br />
-          </div>
-          <div className='py-5'>
-            <input className='rounded lg:w-80 w-full h-[35px] lg:h-[40px]' type='email' name='' id='' placeholder='Email' /> <br />
-          </div>
-          <input className='rounded lg:w-80 w-full h-[35px]' type='password' name='' id='' placeholder='Contraseña' />
-          <input className='rounded lg:w-80 w-full h-[35px]' type='password' name='' id='' placeholder='Confirmar Contraseña' />
         </form>
 
-        <button className='bg-blue border-solid rounded-3xl mt-20 w-full text-xl bg-slate-500 h-[50px]'>Ingresar</button>
-        <button className='bg-blue border-solid rounded-3xl mt-10 w-full text-xl bg-slate-500 h-[50px]'>Registrar</button>
+        <button className='bg-blue border-solid rounded-3xl mt-10 w-full text-xl bg-slate-500 h-[50px]' onClick={handleRegisterForm}>Registrar</button>
       </div>
     </div>
   )

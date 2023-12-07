3662f10942634cebc65632e8b8b82af140945196
lucerocamila <lucerogonzalez1997@gmail.com>
2023-12-07 06:40:41 -0300

    style: Implement animations and adjust styles for improved visual appeal

--- client/src/components/NewsCard/index.jsx
+++ client/src/components/NewsCard/index.jsx
@@ -1,11 +1,12 @@
-// CardComponent.js
+// NewsCard.js
 import React from 'react';
+import '../../index.css';
 
-const NewsCard = ({ news }) => {
+const NewsCard = ({ news, isZoomed }) => {
   return (
-    <div className='flex-col max-w-sm lg:max-w-none lg:w-[20rem] mx-4 w-full h-[12rem] lg:h-[24rem] px-2 py-4 bg-gray-50 drop-shadow-md rounded-md flex justify-around items-end text-center text-neutral-900'>
-              <img src={news.image} alt={news.new} className='w-full h-[12rem] object-cover mb-2 rounded-md' />
-<div className='w-56 h-20 flex-col text-left mb-8 '>
+    <div className={`flex-col max-w-sm lg:max-w-none lg:w-[20rem] mx-4 w-full h-[12rem] lg:h-[19rem] px-2 py-4 bg-white drop-shadow-md rounded-md flex justify-around items-start text-center text-neutral-900 ${isZoomed ? 'lift-up is-zoomed' : ''}`}>
+      <img src={news.image} alt={news.new} className={`self-center w-full lg:max-w-[17rem] lg:max-h-[10rem] h-[12rem] object-cover mb-5 rounded-lg ${isZoomed ? 'scale-up is-zoomed' : ''}`} />
+      <div className='w-56 h-20 flex-col text-left mb-[6rem]'>
         <h2 className='text-title font-title font-bold'>{news.category}</h2>
         <p className='text-sans font-subtitle'>{news.detail}</p>
         <p className='text-xs text-gray-500 mt-2'>{new Date(news.dateTime).toLocaleString()}</p>

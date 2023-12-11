c9db6926ad4a66c0011cea28ceeeac8150d8f084
guillenec <guillermoneculqueo@gmail.com>
2023-12-11 19:28:48 -0300

    feat:se crea componente paqueteria vacio

--- client/src/components/search/index.jsx
+++ client/src/components/search/index.jsx
@@ -1,58 +1,56 @@
-import { useNavigate } from 'react-router-dom'
 import React, { useEffect, useState } from 'react'
 import { IoSearch } from 'react-icons/io5'
-import packages from './dbPruebas'
 import filtersProductsByKeyword from './filtersProductsByKeyword'
 
-const Search = () => {
-  const navigate = useNavigate()
+const Search = ({ allVisitas, nameColumn, handleSearch }) => {
   const [searchTerm, setSearchTerm] = useState('')
-  const [redirect, setRedirect] = useState(false)
+  const [buscando, setbuscando] = useState(false)
 
   useEffect(() => {
-    if (redirect === true) {
+    if (buscando === true) {
       setTimeout(() => {
-        navigate('/search')
-        setRedirect(false)
+        setbuscando(false)
       }, 3000)
     }
-  }, [redirect])
-
-  const handleSearch = () => {
-    const filteredData = filtersProductsByKeyword(packages, searchTerm)
-    console.log('Foods Filter Keyword ->', filteredData)
-    setRedirect(false)
-    setSearchTerm('...buscando')
-    setTimeout(() => {
-      setSearchTerm('')
-    }, 1000)
-  }
+  }, [buscando])
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    handleSearch()
+    const filteredData = filtersProductsByKeyword({ products: allVisitas, nameColumn, searchTerm })
+    handleSearch(filteredData)
+    setbuscando(true)
+    setSearchTerm('...buscando')
+    setTimeout(() => {
+      setSearchTerm('')
+    }, 300)
   }
 
   return (
     <div className='containerSear w-full h-auto flex justify-center items-center'>
       {
-        redirect
+        buscando
           ? (
-            <p className='redirect '> Redirigiendo...</p>
+            <div className='bg-gradient-to-r from-green-500 via-green-700 to-blue-400 rounded-[50px] p-1 font-titulo'>
+              <div className=' h-[50px] w-[268px] rounded-[50px] bg-white outline-none font-parrafo font-medium text-lg px-8 flex items-center justify-center'>
+                <p className='redirect '> Buscando...</p>
+              </div>
+            </div>
             )
           : (
             <form onSubmit={handleSubmit} className='relative'>
-              <input
-                type='text'
-                name='text'
-                className='input h-[45px] w-[278px] rounded-[50px] border-2 border-colorCustom1 bg-white outline-none font-parrafo font-medium text-sm px-7'
-                value={searchTerm}
-                onChange={(event) => setSearchTerm(event.target.value)}
-                placeholder='Buscador'
-              />
-              <button type='submit' className='icon absolute left-2 top-[14px] text-lg'>
-                <IoSearch />
-              </button>
+              <div className='bg-gradient-to-r from-green-500 via-green-700 to-blue-400 rounded-[50px] p-1 font-titulo text-lg '>
+                <input
+                  type='text'
+                  name='text'
+                  className='input h-[50px] w-[268px] rounded-[50px] bg-white outline-none font-parrafo font-medium text-sm px-8'
+                  value={searchTerm}
+                  onChange={(event) => setSearchTerm(event.target.value)}
+                  placeholder='Buscador'
+                />
+                <button type='submit' className='icon absolute h-[100%] left-2 top-0 text-xl flex justify-center items-center'>
+                  <IoSearch />
+                </button>
+              </div>
             </form>
             )
       }
--- client/src/pages/History/components/Historial/index.jsx
+++ client/src/pages/History/components/Historial/index.jsx
@@ -7,17 +7,20 @@ import Paginacion from './components/Paginacion'
 import axios from 'axios'
 import useVicitStore from '../../../../store/VisitStore/VisitStore'
 import { endpoints } from '../../../../constants/api'
+import Search from '../../../../components/search'
 
 const Historial = () => {
   const { setActiveIndex } = useNavStore()
   const [currentPage, setCurrentPage] = useState(1)
 
   const { visitas, setVisitas } = useVicitStore()
+  const [filterVisitas, setFilterVisitas] = useState([])
 
   useEffect(() => {
     axios.get(endpoints.visitas)
       .then(res => {
         setVisitas(res.data.data)
+        setFilterVisitas(res.data.data)
       })
       .catch(err => console.log(err))
   }, [setVisitas])
@@ -28,10 +31,10 @@ const Historial = () => {
   }, [])
 
   const itemsPerPage = 5
-
   const indexOfLastItem = currentPage * itemsPerPage
   const indexOfFirstItem = indexOfLastItem - itemsPerPage
-  const currentItems = visitas?.slice(indexOfFirstItem, indexOfLastItem)
+  // const currentItems = visitas?.slice(indexOfFirstItem, indexOfLastItem)
+  const currentItems = filterVisitas?.slice(indexOfFirstItem, indexOfLastItem)
 
   const paginate = (action) => {
     if (action === 'next') {
@@ -41,27 +44,33 @@ const Historial = () => {
     }
   }
 
+  const handleSearch = (filterData) => {
+    setFilterVisitas(filterData)
+  }
+
   console.log('Vicitas --> ', visitas)
 
   return (
     <div
-      className='h-full w-[95%] sm:w-[90%] md:w-[90%] lg:w-[98%] xl:w-[98%] 2xl:w-[100%] lg:flex flex-col lg:justify-center lg:items-center overflow-x-scroll 2xl:overflow-auto scrollbar pb-2'
+      className='h-full w-[95%] sm:w-[90%] md:w-[90%] lg:w-[98%] xl:w-[98%] 2xl:w-[100%] lg:flex flex-col lg:justify-center lg:items-center overflow-x-scroll 2xl:overflow-auto scrollbar pb-2 gap-4'
 
     >
-      <div className='w-[1000px] 2xl:w-[100%] grid grid-cols-5  rounded-lg border-2 overflow-hidden border-colorCustom1 text-colorCustom4 font-titulo font-medium text-base'>
-        <div className='bg-colorCustom1 py-2'>
+      <Search allVisitas={visitas} nameColumn='visitorFullName' handleSearch={handleSearch} />
+      {/* aca puedo componetizar mas */}
+      <div className='mt-3 w-[1000px] 2xl:w-[100%] grid grid-cols-5  rounded-lg border-2 overflow-hidden bg-gradient-to-r from-green-500 via-green-700 to-blue-400  text-colorCustom4 font-titulo font-medium text-base'>
+        <div className=' py-2'>
           <h2 className='w-full text-center'>Empleado</h2>
         </div>
-        <div className='bg-colorCustom1 py-2'>
+        <div className=' py-2'>
           <span>Estado</span>
         </div>
-        <div className='bg-colorCustom1 py-2'>
+        <div className=' py-2'>
           <span>Publicado</span>
         </div>
-        <div className='bg-colorCustom1 py-2'>
+        <div className=' py-2'>
           <span>Direccion</span>
         </div>
-        <div className='bg-colorCustom1 py-2 rounded-tr-md'>
+        <div className=' py-2 rounded-tr-md'>
           <span>Nombre</span>
         </div>
 

26db42c890cd18feb9b2dfa135d77615e3afcecf
guillenec <guillermoneculqueo@gmail.com>
2023-12-11 01:28:51 -0300

    feat: componente Pagina NotFound actualizada de pruebas

--- client/src/pages/NotFound/NotFound.jsx
+++ client/src/pages/NotFound/NotFound.jsx
@@ -5,13 +5,13 @@ const NotFound = () => {
   return (
     <section className='w-full max-h-full h-[calc(100vh-100px)] mt-0 flex flex-col justify-center items-center bg-greenCustom gap-3'>
       <h1 className='font-titulo text-[100px] font-bold text-whiteCustom1'>404 </h1>
-      <h2 className='font-parrafo text-[22px] font-semibold text-whiteCustom1'>Opps! That page can't be found</h2>
-      <p className='font-parrafo text-[18px] font-normal text-whiteCustom1'>The page you are looking for it maybe deleted</p>
+      <h2 className='font-parrafo text-[22px] font-semibold text-whiteCustom1'>¡Ups! esa pagina no se puede encontrar</h2>
+      <p className='font-parrafo text-[18px] font-normal text-whiteCustom1'>La página que estás buscando no existe o fue eliminada.</p>
       <Link
-        to='/'
+        to='/home'
         className='mt-[1rem] font-parrafo text-[1rem] font-semibold border-2 border-whiteCustom1 text-whiteCustom1 px-[1.5rem] py-[.8rem] rounded-[7px] hover:text-oranjeCustom hover:bg-whiteCustom1 hover:border-oranjeCustom transition-all duration-200 ease-in'
       >
-        Go To Home
+        Ir al Home
       </Link>
     </section>
   )

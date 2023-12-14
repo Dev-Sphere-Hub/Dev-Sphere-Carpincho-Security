17f750ad28e78a10e89081cde4fcfa8514fbd36c
lucerocamila <lucerogonzalez1997@gmail.com>
2023-12-14 18:47:11 -0300

    style: update styles on several components

--- client/src/components/NewsCard/index.jsx
+++ client/src/components/NewsCard/index.jsx
@@ -1,16 +1,18 @@
 // NewsCard.js
 import React from 'react';
-import '../../index.css';
+import { IoIosArrowForward } from "react-icons/io";
 
-const NewsCard = ({ news, isZoomed }) => {
+const NewsCard = ({ news, onClick }) => {
   return (
-    <div className={`flex-col max-w-sm lg:max-w-none lg:w-[20rem] mx-4 w-full h-[12rem] lg:h-[19rem] px-2 py-4 bg-white drop-shadow-md rounded-md flex justify-around items-start text-center text-neutral-900 ${isZoomed ? 'lift-up is-zoomed' : ''}`}>
-      <img src={news.image} alt={news.new} className={`self-center w-full lg:max-w-[17rem] lg:max-h-[10rem] h-[12rem] object-cover mb-5 rounded-lg ${isZoomed ? 'scale-up is-zoomed' : ''}`} />
-      <div className='w-56 h-20 flex-col text-left mb-[6rem]'>
-        <h2 className='text-title font-title font-bold'>{news.category}</h2>
-        <p className='text-sans font-subtitle'>{news.detail}</p>
-        <p className='text-xs text-gray-500 mt-2'>{new Date(news.dateTime).toLocaleString()}</p>
+    <div className="flex w-[21rem] my-4 lg:m-4 p-3 pb-5 bg-colorCustom6 drop-shadow-md rounded-xl items-start text-center text-neutral-900" onClick={() => onClick(news)}>
+      <img src="https://source.unsplash.com/featured/?warning" alt={news.new} className="mx-2 self-center h-full rounded-lg max-h-[2.7rem]" />
+      <div className="flex-col text-left">
+        <p className="text-gray-600 text-sm">{`${news.author.fullName}`}</p>
+        <h2 className="text-title font-title font-bold">{news.category}</h2>
+        {/* <p className="text-sans font-subtitle">{news.detail}</p>       */}
+        <p className="text-xs text-gray-600">{new Date(news.date).toLocaleString()}</p>
       </div>
+      <IoIosArrowForward className="ml-auto self-center text-2xl text-gray-500" />
     </div>
   );
 };
--- client/src/pages/GuardianJournal/index.jsx
+++ client/src/pages/GuardianJournal/index.jsx
@@ -1,29 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import Carousel from '../../components/Carousel';
+import React, { useState, useEffect } from 'react';
 import useNavStore from '../../store/NavStore/navStore';
+import NewsCard from '../../components/NewsCard';
 import axios from 'axios';
 import { endpoints } from '../../constants/api';
+import { CiCirclePlus } from "react-icons/ci";
 
 const GuardianJournal = () => {
   const { setActiveIndex } = useNavStore();
   const [formData, setFormData] = useState({
-    dateTime: '',
     category: '',
     detail: ''
   });
 
   const [novedades, setNovedades] = useState([]);
+  const [selectedNews, setSelectedNews] = useState(null);
   const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
-    //  novedades
     axios.get(endpoints.nuevos)
       .then(res => {
         setNovedades(res.data.data);
       })
       .catch(err => console.log(err));
-  }, []);            console.log(novedades);
-
+  }, []);
 
   useEffect(() => {
     setActiveIndex('reportes');
@@ -40,162 +39,57 @@ const GuardianJournal = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Enviar datos de formData para registrar novedad
     axios.post(endpoints.nuevos, formData)
       .then(res => {
         console.log('Novedad registrada con éxito.');
         setNovedades([...novedades, formData]);
       })
       .catch(err => console.log(err));
-console.log(formData);
 
-    // Limpiar el formulario 
     setFormData({
-      dateTime: '',
       category: '',
       detail: ''
     });
 
-    // Ocultar el modal
     setShowModal(false);
   };
 
+  const handleCardClick = (news) => {
+    setSelectedNews(news);
+  };
+
   return (
-    <div id='novedades' className='w-[100%] min-h-[500px] h-auto xl:min-w-[1280px] flex flex-col items-center gap-[5rem] border-2 p-2 rounded-md '>
-      <h2 className='self-center lg:self-start text-3xl w-[100%] lg:w-[860px] font-bold mx-auto lg:text-left'>Novedades</h2>
-      <div className='hidden lg:block'>
-        <Carousel />
-      </div>
-
-      {showModal && (
-        <div className='fixed inset-0 flex items-center justify-center z-50'>
-          <div className='absolute inset-0 bg-gray-900 opacity-75' />
-          <div className='relative max-w-md w-full bg-white rounded-lg p-6'>
-            <h2 className='text-2xl font-bold mb-4'>Registrar Novedad</h2>
-            <form onSubmit={handleSubmit}>
-              <div className='mb-4'>
-                <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='dateTime'>
-                  Fecha / Hora:
-                </label>
-                <input
-                  type='datetime-local'
-                  id='dateTime'
-                  name='dateTime'
-                  value={formData.dateTime}
-                  onChange={handleInputChange}
-                  className='w-full bg-gray-200 border-2 border-gray-200 rounded-md py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-blue-500'
-                  required
-                />
-              </div>
-              <div className='mb-4'>
-                <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='category'>
-                  Categoría:
-                </label>
-                <select
-                  id='category'
-                  name='category'
-                  value={formData.category}
-                  onChange={handleInputChange}
-                  className='w-full bg-gray-200 border-2 border-gray-200 rounded-md py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-blue-500'
-                  required
-                >
-                  <option value='' disabled>
-                    Seleccionar Categoría
-                  </option>
-                  <option value='Emergencias'>Emergencias</option>
-                  <option value='Eventos Destacados'>Eventos Destacados</option>
-                  <option value='Personas No Autorizadas'>Personas No Autorizadas</option>
-                  <option value='Vehiculo No Autorizado'>Vehículo No Autorizado</option>
-                </select>
-              </div>
-              <div className='mb-4'>
-                <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='detail'>
-                  Detalle:
-                </label>
-                <textarea
-                  id='detail'
-                  name='detail'
-                  value={formData.detail}
-                  onChange={handleInputChange}
-                  className='w-full bg-gray-200 border-2 border-gray-200 rounded-md py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-blue-500'
-                  required
-                />
-              </div>
-              <div className='flex items-center justify-end mt-4'>
-                <button
-                  className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-                  type='submit'
-                >
-                  Registrar Novedad
-                </button>
-                <button
-                  className='ml-2 bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-                  onClick={() => setShowModal(false)}
-                >
-                  Cancelar
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      )}
-
-      <div className='flex flex-col gap-4 md:flex-row flex-wrap md:max-w-[80%] md:m-auto justify-center'>
+    <div className='flex justify-evenly text-center w-[100%] min-h-[500px]h-auto xl:min-w-[1280px]'>
+      <div className='flex-col max-w-[500px] justify-center'>
+        <h2 className='mx-auto'>Novedades</h2>
         {novedades.map((novedad, index) => (
-          <div
+          <NewsCard
             key={index}
-            className='max-w-sm mx-4 w-full h-[19rem] px-2 py-4 bg-gray-50 drop-shadow-md rounded-md flex justify-around items-center text-center text-neutral-900'
-          >
-            <div className='w-56 h-20 flex-col text-left'>
-              <img src="" alt="" />
-              <h2 className='text-title font-title font-bold'>{novedad.category}</h2>
-              <p className='text-sans font-subtitle'>{novedad.detail}</p>
-              <p className='text-sans font-subtitle mt-2'>{`Reportado por: ${novedad.author.fullName}`}</p>
-              <p className='text-xs text-gray-500 mt-2'>{`Fecha: ${(novedad.date)}`}</p>
-            </div>
-          </div>
+            news={novedad}
+            isZoomed={selectedNews === novedad}
+            onClick={() => handleCardClick(novedad)}
+          />
         ))}
+      </div>
+         <div className='hidden lg:block'>
+         <div className='flex flex-col w-[30rem] bg-[#EDEDED] h-[36rem] mx-auto border-2 p-2 rounded-md'>
+  {selectedNews ? (
+    <div className='mx-auto text-neutral-900'>
+      <img src="https://source.unsplash.com/featured/?warning" alt={selectedNews.new} className='w-full max-w-[20rem] h-[11rem] object-cover mb-5 rounded-xl mx-auto' />
+      <div className='flex-wrap px-2 w-full h-20 flex-col text-left mx-auto'>
+        <p className='text-sans font-subtitle mt-2'>{`Reportado por: ${selectedNews.author.fullName}`}</p>
+        <h2 className='text-title font-title font-bold'>{selectedNews.category}</h2>
+        <p className=' text-sans font-subtitle'>{selectedNews.detail}</p>
+        <p className='text-xs text-gray-500 mt-2'>{new Date(selectedNews.date).toLocaleString()}</p>
+      </div>
+    </div>
+  ) : (
+    <div className='h-[12rem] rounded-lg flex flex-col justify-center items-center cursor-pointer hover:bg-gray-300' onClick={() => setShowModal(true)}>
+      <p className="text-center">Selecciona una novedad para ver más información detallada.</p>
+    </div>
+  )}
+</div>
 
-        <div
-          className='max-w-[22rem] h-[13.5rem]rounded-lg flex justify-center items-center self-center cursor-pointer hover:bg-gray-300'
-          onClick={() => setShowModal(true)}
-        >
-          <div className='hidden lg:block'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='32'
-              height='32'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              className='feather feather-plus'
-            >
-              <line x1='12' y1='5' x2='12' y2='19' />
-              <line x1='5' y1='12' x2='19' y2='12' />
-            </svg>
-          </div>
-
-          <div className='w-16 h-16 lg:hidden bg-gray-600 rounded-full flex justify-center items-center cursor-pointer hover:bg-gray-300'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='32'
-              height='32'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              className='feather feather-plus'
-            >
-              <line x1='12' y1='5' x2='12' y2='19' />
-              <line x1='5' y1='12' x2='19' y2='12' />
-            </svg>
-          </div>
-        </div>
       </div>
     </div>
   );

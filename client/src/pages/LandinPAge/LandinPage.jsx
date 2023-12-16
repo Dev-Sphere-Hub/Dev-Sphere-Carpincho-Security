b8f3f701601b5ad4450a21b66771afc8cc1e2788
guillenec <guillermoneculqueo@gmail.com>
2023-12-15 21:45:35 -0300

    fix: borrando estilos landing

--- client/src/pages/LandinPAge/LandinPage.jsx
+++ client/src/pages/LandinPAge/LandinPage.jsx
@@ -12,7 +12,7 @@ const LandinPage = () => {
       </section>
 
       <section
-        className='contenudoTextios w-[100%] md:w-[50%] md:h-[100%] md:absolute md:top-0 md:left-0 md:flex md:flex-col md:justify-center md:items-center gap-5 bg-[#6935b75e] shadow-[0_0_0_6px_rgba(35,17,44,0.05)] backdrop-blur-[6px]'
+        className='contenudoTextios w-[100%] md:w-[50%] md:h-[100%] md:absolute md:top-0 md:left-0 md:flex md:flex-col md:justify-center md:items-center gap-5 bg-green-300 shadow-[0_0_0_6px_rgba(35,17,44,0.05)] backdrop-blur-[6px]'
       >
         <h1 className='font-bold text-white fonrt-titulo text-5xl px-1'>Bienvenido a Carpincho Security !!!</h1>
         <p className='font-normal font-parrafo text-md text-left text-yellow-100 px-4'>Carpincho Security redefine la seguridad en 🏢 edificios, 🏡 countries y establecimientos al proporcionar un sistema integral de <strong>control de acceso</strong>. Diseñada para brindar tranquilidad, nuestra aplicación permite a los profesionales de seguridad tener un control total sobre las <strong>entradas, salidas, paquetería y entregas</strong>. Con Carpincho Security, cada interacción queda registrada, brindando una gestión eficiente y segura. ¡<strong>Tu paz de mente</strong> es nuestra prioridad! 🛡️🔒</p>

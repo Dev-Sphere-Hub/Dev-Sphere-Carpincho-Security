2bd3f4deaa76a7717679a47a84f5a0af80293478
lucerocamila <lucerogonzalez1997@gmail.com>
2023-12-17 23:00:24 -0300

    feat: Enhance form validations in LoginForm and Register components

--- client/src/pages/LoginForm/index.jsx
+++ client/src/pages/LoginForm/index.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useForm } from 'react-hook-form'
-import { useAuthStore } from '../../store/AuthStore/AuthStore'
-import { endpoints } from '../../constants/api'
-import { jwtDecode } from 'jwt-decode'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { useAuthStore } from '../../store/AuthStore/AuthStore';
+import { endpoints } from '../../constants/api';
+import { jwtDecode } from 'jwt-decode';
+import InputForm from '../../components/InputForm';
 
 const LoginForm = () => {
-  const localStorage = window.localStorage
-  const { setTokenDesifred, setToken } = useAuthStore()
-  const Navigate = useNavigate()
+  const localStorage = window.localStorage;
+  const { setTokenDesifred, setToken } = useAuthStore();
+  const Navigate = useNavigate();
 
-  const { register, handleSubmit, formState: { errors } } = useForm()
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
   const handleLogin = async (data) => {
     try {
@@ -20,66 +21,74 @@ const LoginForm = () => {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
-      })
+      });
 
-      const responseData = await res.json()
+      const responseData = await res.json();
 
       if (res.status === 200) {
-        const decodedToken = jwtDecode(responseData.data.token)
-        localStorage.setItem('token', JSON.stringify(decodedToken))
-        setToken(responseData.data.token)
-        setTokenDesifred(decodedToken)
-        Navigate('/historial')
+        const decodedToken = jwtDecode(responseData.data.token);
+        localStorage.setItem('token', JSON.stringify(decodedToken));
+        setToken(responseData.data.token);
+        setTokenDesifred(decodedToken);
+        Navigate('/historial');
       } else {
-        throw new Error(responseData.message)
+        throw new Error(responseData.message);
       }
     } catch (error) {
-      console.error(error.message)
+      console.error(error.message);
     }
-  }
+  };
 
   const handleRegister = () => {
-    Navigate('/register')
-  }
+    Navigate('/register');
+  };
 
   return (
-    <div className='relative bg-colorCustom1 w-[100%] px-6 lg:bg-slate-400 h-screen  p-0 flex flex-col lg:flex-row lg:justify-around gap-8 min-w-[300px]'>
-      <div className='lg:flex-col lg:self-center'>
-        <div className='pt-9 text-black text-5xl'>
-          <h1>Logo</h1>
-        </div>
-        <div className='pt-6 text-lg lg:flex'>
-          <p>Carpincho Segurity</p>
-        </div>
-      </div>
+    <div className='relative bg-colorCustom1 w-[100%] px-6 lg:bg-slate-400 h-screen p-0 flex flex-col lg:flex-row lg:justify-around gap-8 min-w-[300px]'>
       <div className='pt-9 lg:self-center'>
         <form onSubmit={handleSubmit(handleLogin)}>
           <div className='py-5'>
-            <input
-              className='rounded lg:w-80 w-full h-[35px] lg:h-[40px]'
+            <InputForm
+              label='Email'
+              placeholder='Escribe tu correo electrónico'
               type='email'
-              name='email'
-              placeholder='Email'
-              {...register('email', { required: 'El email es requerido' })}
-            /> <br />
-            {errors.email && <span style={{ color: 'red' }}>{errors.email.message}</span>}
+              register={register('email', {
+                required: 'Por favor, ingresa tu correo electrónico',
+                pattern: {
+                  value: /^\S+@\S+$/i,
+                  message: 'Correo electrónico inválido'
+                }
+              })}
+              errorType={errors?.email}
+              errorMessage={errors?.email?.message}
+            />
           </div>
-          <input
-            className='rounded lg:w-80 w-full h-[35px]'
+          <InputForm
+            label='Contraseña'
+            placeholder='Ingresa tu contraseña'
             type='password'
-            name='password'
-            placeholder='Contraseña'
-            {...register('password', { required: 'Ingresa la contraseña' })}
+            register={register('password', {
+              required: 'Por favor, ingresa tu contraseña',
+              minLength: {
+                value: 6,
+                message: 'La contraseña debe tener al menos 6 caracteres'
+              }
+            })}
+            errorType={errors?.password}
+            errorMessage={errors?.password?.message}
           />
-          {errors.password && <p style={{ color: 'red' }}>{errors.password.message}</p>}
           <p className='m-[15px] pl-[155px]'>Recuperar contraseña</p>
 
-          <button className='bg-blue border-solid rounded-3xl mt-20 w-full text-xl bg-slate-500 h-[50px]' type='submit'>Ingresar</button>
-          <button className='bg-blue border-solid rounded-3xl mt-10 w-full text-xl bg-slate-500 h-[50px]' onClick={handleRegister}>Registrar</button>
+          <button className='bg-blue border-solid rounded-3xl mt-20 w-full text-xl bg-slate-500 h-[50px]' type='submit'>
+            Ingresar
+          </button>
+          <button className='bg-blue border-solid rounded-3xl mt-10 w-full text-xl bg-slate-500 h-[50px]' onClick={handleRegister}>
+            Registrar
+          </button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
